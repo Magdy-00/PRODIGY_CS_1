110339174ac11f8c6edf0f066e336c7b52bae9ea
Magdy Khalil <114831143+Magdy-00@users.noreply.github.com>
2024-07-07 17:56:54 +0300

    Add files via upload

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -10,7 +10,10 @@ def EncryptedNumbers(Message:str,shift:int):
     transformed_message = transform(Message)
     encryptedNumbers = []
     for i in transformed_message:
-        encryptedNumbers.append((int(i) + shift) % 26)
+        if isinstance(i, int):  # Only shift numeric characters
+            encryptedNumbers.append((i + shift) % 26)
+        else:
+            encryptedNumbers.append(i)  # Preserve spaces and other characters
     return encryptedNumbers
 
 
@@ -18,7 +21,10 @@ def DecryptedNumbers(Message:str,shift:int):
     transformed_message = transform(Message)
     decryptedNum=[]
     for i in transformed_message:
-        decryptedNum.append((int(i) - shift) %26 )
+        if isinstance(i, int):  # Only shift numeric characters
+            decryptedNum.append((i - shift) % 26)
+        else:
+            decryptedNum.append(i)  # Preserve spaces and other characters
     return decryptedNum
 
 
@@ -29,14 +35,16 @@ def Decryption(Message: str, shift: int):
         0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h',
         8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p',
         16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x',
-        24: 'y', 25: 'z'
+        24: 'y', 25: 'z' 
     }
 
     dec = ''
     for num in temp2:
-        if num in numberChar2:
+        if num ==-1:
+            dec+=" "
+        elif num in numberChar2:
             dec += numberChar2[num]  # Directly add the character to the string
-
+            
     return dec
 
 
@@ -54,7 +62,7 @@ def Encryption(Message:str,shift:int):
         if num in numberChar:
             Encrypted.append(str(numberChar[num]))  # Append the transformed value to the list
         else:
-            print("Please Enter a vaild number")
+            Encrypted.append(num)
 
     for i in Encrypted: #to print the encypted message as a string
         enc=enc+i
@@ -68,18 +76,19 @@ def transform(Message)->str:
         'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7,
         'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15,
         'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23,
-        'y': 24, 'z': 25
+        'y': 24, 'z': 25 
     
 }
     transformed_message = []
 # Iterate through each character in the input message
     for char in Message:
-
-        if char in charNumber:
+        if char ==" ":
+            transformed_message.append(" ")
+        elif char in charNumber:
             transformed_message.append(int(charNumber[char]))  # Append the transformed value to the list
 
-        # else:
-        #     transformed_message.append(char)
+        else:
+            transformed_message.append(char)
 
     return transformed_message
 # End of transform Function
